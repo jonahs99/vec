18f7d46ddde98817998c2f064f1b5bd49b938aaf
Jonah Simpson <jonahs99@gmail.com>
2018-04-11 20:32:20 -0400

    Add zero-vector and unit function

--- math.go
+++ math.go
@@ -4,6 +4,13 @@ import (
 	"math"
 )
 
+var zero = Vec{0, 0}
+
+// Zero returns the zero vector
+func Zero() Vec {
+	return zero
+}
+
 // Mag2 calculates the squared magnitude
 func Mag2(v Vec) float64 {
 	return math.Pow(v.X, 2) + math.Pow(v.Y, 2)
@@ -37,6 +44,16 @@ func Dist2(v1, v2 Vec) float64 {
 // Dist calculates the magnitude of the difference vector
 func Dist(v1, v2 Vec) float64 {
 	return math.Sqrt(math.Pow(v1.X-v2.X, 2) + math.Pow(v1.Y-v2.Y, 2))
+}
+
+// Unit returns a unit-size vector in the same direction
+// Returns zero on zero
+func Unit(v Vec) Vec {
+	if v == zero {
+		return zero
+	}
+	mag := Mag(v)
+	return Div(v, mag)
 }
 
 // Norm returns a counter-clockwise points normal
